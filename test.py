4e57929416cd40a91048886ed8f689da672bd27e
amshekar <amshekar@hotmail.com>
2017-08-05 21:14:48 +0530

    python days

---
+++ test.py
@@ -0,0 +1,19 @@
+import  random
+import  sys
+import os
+print("Hello world")
+name="shekar"
+print(name.__len__())
+name=1001
+print(name)
+print(name.__sizeof__())
+print(34/5)
+print(1+3-3*2)
+print(5*2)
+print(5-2)
+print(5+2)
+print(5/2)
+print(5%2)
+print(5**2)
+print(5//2)
+print("test"+"2")
